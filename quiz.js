79dd2c6adad23e714a2da0bc8b8d381b737128f1
IhorPronin <igorpronin1212@gmail.com>
2019-03-22 22:08:26 +0200

    Add scope and closure quiz

--- quiz.js
+++ quiz.js
@@ -5,3 +5,27 @@ function hi(count){
 setTimeout(() => hi(2), 5000);
 setTimeout(() => hi(1), 0);
 console.log('Hello sync');
+
+// scope example
+var a = 1;
+function scope(){
+    a = 2
+    function scope1(){
+        a = 3
+    }
+}
+scope()
+console.log(a)
+
+// scope advanced
+
+function closure(){
+    var c = 1
+    console.log('I am here')
+    return function (b){
+        console.log(`I am here ${c} ${b}`)
+    }
+}
+
+var closureFunc = closure();
+closureFunc(2)
